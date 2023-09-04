1fbfdef2580607d472f62793046db5047695e4d0
hlh112 <84497856+hlh112@users.noreply.github.com>
2023-09-04 08:44:14 +0800

    Update pre_trained_schema.js

--- src/data/pre_trained_schema.js
+++ src/data/pre_trained_schema.js
@@ -2,7 +2,7 @@ let schema = [
     {
         extractorType: 'Receipt',
         PreTrainedFields : [
-            {field_name: 'merchant_name', default_field_name: 'merchant_name', field_status: false},
+            {field_name: 'merchant_name', default_field_name: 'merchant_name', field_status: true},
             {field_name: 'total_amount', default_field_name: 'total_amount', field_status: true},
             {field_name: 'date', default_field_name: 'date', field_status: true},
             {field_name: 'address', default_field_name: 'address', field_status: false},
@@ -10,7 +10,7 @@ let schema = [
             {field_name: 'mall_name', default_field_name: 'mall_name', field_status: false},
             {field_name: 'reference_number', default_field_name: 'reference_number', field_status: false},
             {field_name: 'product_name', default_field_name: 'product_name', field_status: false},
-            {field_name: 'store_number', default_field_name: 'store_number', field_status: false},
+            {field_name: 'store_number', default_field_name: 'store_number', field_status: true},
             {field_name: 'payment_method', default_field_name: 'payment_method', field_status: false}
         ],
         CustomFields : [
@@ -53,6 +53,12 @@ let schema = [
     {
         extractorType: 'Invoice',
         PreTrainedFields : [
+            {field_name: 'total_amount', default_field_name: 'bank_name', field_status: true},
+            {field_name: 'reference_number', default_field_name: 'reference_number', field_status: false},
+            {field_name: 'due_date', default_field_name: 'due_date', field_status: false},
+            {field_name: 'customer_address', default_field_name: 'customer_address', field_status: false},
+            {field_name: 'invoice_number', default_field_name: 'invoice_number', field_status: true},
+            {field_name: 'invoice_date', default_field_name: 'invoice_date', field_status: true},
             {field_name: 'bank_name', default_field_name: 'bank_name', field_status: false},
         ],
         CustomFields : [
@@ -61,7 +67,13 @@ let schema = [
     {
         extractorType: 'Bank Statement',
         PreTrainedFields : [
-            {field_name: 'bank_name', default_field_name: 'bank_name', field_status: false},
+            {field_name: 'total_amount', default_field_name: 'bank_name', field_status: true},
+            {field_name: 'reference_number', default_field_name: 'reference_number', field_status: true},
+            {field_name: 'due_date', default_field_name: 'due_date', field_status: false},
+            {field_name: 'customer_address', default_field_name: 'customer_address', field_status: false},
+            {field_name: 'invoice_number', default_field_name: 'invoice_number', field_status: false},
+            {field_name: 'invoice_date', default_field_name: 'invoice_date', field_status: true},
+            {field_name: 'bank_name', default_field_name: 'bank_name', field_status: true},
         ],
         CustomFields : [
         ],
@@ -69,7 +81,13 @@ let schema = [
     {
         extractorType: 'ID card',
         PreTrainedFields : [
-            {field_name: 'full_name', default_field_name: 'bank_name', field_status: false},
+            {field_name: 'full_name_chinese', default_field_name: 'full_name_chinese', field_status: true},
+            {field_name: 'full_name_english', default_field_name: 'bank_name_english', field_status: true},
+            {field_name: 'date_of_birth', default_field_name: 'date_of_birth', field_status: true},
+            {field_name: 'expiry_date', default_field_name: 'expiry_date', field_status: true},
+            {field_name: 'gender', default_field_name: 'gender', field_status: true},
+            {field_name: 'nationality', default_field_name: 'nationality', field_status: true},
+            {field_name: 'address', default_field_name: 'address', field_status: false}
         ],
         CustomFields : [
         ],
@@ -77,7 +95,13 @@ let schema = [
     {
         extractorType: 'Passport',
         PreTrainedFields : [
-            {field_name: 'full_name', default_field_name: 'bank_name', field_status: false},
+            {field_name: 'full_name_chinese', default_field_name: 'full_name_chinese', field_status: true},
+            {field_name: 'full_name_english', default_field_name: 'bank_name_english', field_status: true},
+            {field_name: 'date_of_birth', default_field_name: 'date_of_birth', field_status: true},
+            {field_name: 'expiry_date', default_field_name: 'expiry_date', field_status: true},
+            {field_name: 'gender', default_field_name: 'gender', field_status: true},
+            {field_name: 'nationality', default_field_name: 'nationality', field_status: true},
+            {field_name: 'address', default_field_name: 'address', field_status: false}
         ],
         CustomFields : [
         ],
@@ -85,7 +109,14 @@ let schema = [
     {
         extractorType: 'Address Proof',
         PreTrainedFields : [
-            {field_name: 'full_name', default_field_name: 'bank_name', field_status: false},
+            {field_name: 'full_name_chinese', default_field_name: 'full_name_chinese', field_status: true},
+            {field_name: 'full_name_english', default_field_name: 'bank_name_english', field_status: true},
+            {field_name: 'date_of_birth', default_field_name: 'date_of_birth', field_status: true},
+            {field_name: 'expiry_date', default_field_name: 'expiry_date', field_status: true},
+            {field_name: 'gender', default_field_name: 'gender', field_status: true},
+            {field_name: 'nationality', default_field_name: 'nationality', field_status: true},
+            {field_name: 'address', default_field_name: 'address', field_status: true}
+
         ],
         CustomFields : [
         ],
@@ -93,7 +124,14 @@ let schema = [
     {
         extractorType: 'Bill of Lading',
         PreTrainedFields : [
-            {field_name: 'bill_number', default_field_name: 'bank_name', field_status: false},
+            {field_name: 'bill_number', default_field_name: 'bill_number', field_status: true},
+            {field_name: 'ship_date', default_field_name: 'ship_date', field_status: true},
+            {field_name: 'shipper_city', default_field_name: 'shipper_city', field_status: true},
+            {field_name: 'shipper_email', default_field_name: 'shipper_email', field_status: true},
+            {field_name: 'carrier_name', default_field_name: 'carrier_name', field_status: true},
+            {field_name: 'carrier_number', default_field_name: 'carrier_number', field_status: true},
+            {field_name: 'shipment_reference', default_field_name: 'shipment_reference', field_status: false},
+            {field_name: 'shipper_address', default_field_name: 'shipper_address', field_status: false}
         ],
         CustomFields : [
         ],
@@ -101,7 +139,14 @@ let schema = [
     {
         extractorType: 'Airway Bill',
         PreTrainedFields : [
-            {field_name: 'bill_number', default_field_name: 'bank_name', field_status: false},
+            {field_name: 'bill_number', default_field_name: 'bill_number', field_status: false},
+            {field_name: 'ship_date', default_field_name: 'ship_date', field_status: true},
+            {field_name: 'shipper_city', default_field_name: 'shipper_city', field_status: true},
+            {field_name: 'shipper_email', default_field_name: 'shipper_email', field_status: true},
+            {field_name: 'carrier_name', default_field_name: 'carrier_name', field_status: true},
+            {field_name: 'carrier_number', default_field_name: 'carrier_number', field_status: true},
+            {field_name: 'shipment_reference', default_field_name: 'shipment_reference', field_status: true},
+            {field_name: 'shipper_address', default_field_name: 'shipper_address', field_status: true}
         ],
         CustomFields : [
         ],
@@ -109,7 +154,14 @@ let schema = [
     {
         extractorType: 'Shipment Label',
         PreTrainedFields : [
-            {field_name: 'bill_number', default_field_name: 'bank_name', field_status: false},
+            {field_name: 'bill_number', default_field_name: 'bill_number', field_status: false},
+            {field_name: 'ship_date', default_field_name: 'ship_date', field_status: true},
+            {field_name: 'shipper_city', default_field_name: 'shipper_city', field_status: true},
+            {field_name: 'shipper_email', default_field_name: 'shipper_email', field_status: true},
+            {field_name: 'carrier_name', default_field_name: 'carrier_name', field_status: true},
+            {field_name: 'carrier_number', default_field_name: 'carrier_number', field_status: true},
+            {field_name: 'shipment_reference', default_field_name: 'shipment_reference', field_status: true},
+            {field_name: 'shipper_address', default_field_name: 'shipper_address', field_status: true}
         ],
         CustomFields : [
         ],
@@ -117,7 +169,12 @@ let schema = [
     {
         extractorType: 'Business Registration',
         PreTrainedFields : [
-            {field_name: 'register_number', default_field_name: 'bank_name', field_status: false},
+            {field_name: 'company_name', default_field_name: 'company_name', field_status: true},
+            {field_name: 'date_of_commencement', default_field_name: 'date_of_commencement', field_status: true},
+            {field_name: 'expiry_date', default_field_name: 'expiry_date', field_status: true},
+            {field_name: 'address', default_field_name: 'address', field_status: true},
+            {field_name: 'business_nature', default_field_name: 'business_nature', field_status: true},
+            {field_name: 'cert_number', default_field_name: 'cert_number', field_status: true}
         ],
         CustomFields : [
         ],
@@ -125,7 +182,12 @@ let schema = [
     {
         extractorType: 'Food License',
         PreTrainedFields : [
-            {field_name: 'register_number', default_field_name: 'bank_name', field_status: false},
+            {field_name: 'company_name', default_field_name: 'company_name', field_status: true},
+            {field_name: 'date_of_commencement', default_field_name: 'date_of_commencement', field_status: true},
+            {field_name: 'expiry_date', default_field_name: 'expiry_date', field_status: true},
+            {field_name: 'address', default_field_name: 'address', field_status: true},
+            {field_name: 'business_nature', default_field_name: 'business_nature', field_status: true},
+            {field_name: 'cert_number', default_field_name: 'cert_number', field_status: true}
         ],
         CustomFields : [
         ],
